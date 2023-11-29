455a2ca617fb92fef9a5263bc647f16b1511c455
Gabo <107078570+GaboE2@users.noreply.github.com>
2023-11-29 17:29:58 -0500

    Add files via upload

---
+++ CVGeneral - Gabriel Baca/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormAndCV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CVGeneral - Gabriel Baca/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace TuApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+ 
+        }
+
+    }
+}
