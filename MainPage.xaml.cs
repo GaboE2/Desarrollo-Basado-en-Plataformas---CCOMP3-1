3ab2c763debedd0bcc3a6fcd970293790d7fd566
Gabo <107078570+GaboE2@users.noreply.github.com>
2023-11-29 16:36:05 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace TuApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCrearCVClicked(object sender, EventArgs e)
+        {
+            // Recoge los datos del formulario
+            string nombre = nombreEntry.Text;
+            string telefono = celularEntry.Text;
+            string correo = correoEntry.Text;
+            string direccion = direccionEntry.Text;
+            string experiencia = experienciaEditor.Text;
+            string formacion = formacionEditor.Text;
+            string idiomas = idiomasEntry.Text;
+            string aptitudes = aptitudesEntry.Text;
+
+            // Crea una instancia de CVPage
+            var cvPage = new CVPage(nombre, telefono, correo, direccion, experiencia, formacion, idiomas, aptitudes);
+            await Navigation.PushAsync(cvPage);
+        }
+
+    }
+}
